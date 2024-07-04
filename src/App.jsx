c9cfe28e85fa2eb40d86ecdf22f829b716a47765
Stacy <pnastya5@mail.ru>
2024-07-04 19:54:45 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { Suspense, useMemo } from "react";
+import { Physics } from "@react-three/rapier";
+import { KeyboardControls } from "@react-three/drei";
+import { Menu } from "./components/Menu";
+
+export const Controls = {
+  forward: "forward",
+  back: "back",
+  left: "left",
+  right: "right",
+  jump: "jump",
+};
+
+function App() {
+  const map = useMemo(
+    () => [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
+    ],
+    []
+  );
+
+  return (
+    <KeyboardControls map={map}>
+      <Canvas shadows camera={{ position: [6, 14, 40], fov: 42 }}>
+        <color attach="background" args={["#dbecfd"]} />
+        <fog attach="fog" args={["#dbecfd", 30, 40]} />
+        <Suspense>
+          <Physics debug>
+            <Experience />
+          </Physics>
+        </Suspense>
+      </Canvas>
+
+      <Menu />
+    </KeyboardControls>
+  );
+}
+
+export default App;
